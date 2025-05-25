5be7eeba03004d01e9a6dd883cb13ab70216febb
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2025-05-25 22:11:14 +0200

    Showing packages user dependencies

--- scripts/emerge-packages.sh
+++ scripts/emerge-packages.sh
@@ -8,8 +8,8 @@ sudo emerge --sync
 # --noreplace: Only build when no version installed
 #
 # Sway: Tiling Window Manager: https://packages.gentoo.org/packages/gui-wm/sway
-# Wofi: (Application) Launcher: https://packages.gentoo.org/packages/gui-apps/wofi
-# Foot: Terminal Emulator: https://packages.gentoo.org/packages/gui-apps/foot
+    # Wofi: (Application) Launcher: https://packages.gentoo.org/packages/gui-apps/wofi
+    # Foot: Terminal Emulator: https://packages.gentoo.org/packages/gui-apps/foot
 sudo emerge \
     --ask \
     --noreplace \
