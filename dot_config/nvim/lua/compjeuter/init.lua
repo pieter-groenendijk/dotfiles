e9c09e049fb07f45b052af4dcbe0917b29ba10aa
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2025-06-13 17:23:31 +0200

    nvim init

---
+++ dot_config/nvim/lua/compjeuter/init.lua
@@ -0,0 +1,7 @@
+-- Tabs
+local tabSize = 4
+
+vim.opt.expandtab = true -- replace ALL tabs with spaces, i.e. fake tabs
+vim.opt.shiftwidth = tabSize -- identation size, i.e. using `<` or `>`
+vim.opt.softtabstop = tabSize -- _fake_ tab size
+
