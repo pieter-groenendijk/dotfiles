0d7f3e151f551fa06217b2552bac3ad2926f1380
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2025-06-13 17:42:58 +0200

    Added lazy + theme

--- dot_config/nvim/lua/compjeuter/init.lua
+++ dot_config/nvim/lua/compjeuter/init.lua
@@ -5,3 +5,46 @@ vim.opt.expandtab = true -- replace ALL tabs with spaces, i.e. fake tabs
 vim.opt.shiftwidth = tabSize -- identation size, i.e. using `<` or `>`
 vim.opt.softtabstop = tabSize -- _fake_ tab size
 
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+      { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
+
+
+require("catppuccin").setup({
+    flavour = "mocha"
+})
+
+vim.cmd.colorscheme("catppuccin")
