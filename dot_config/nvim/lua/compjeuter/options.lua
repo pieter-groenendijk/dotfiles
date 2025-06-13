a2071d080a9592b0915d689384377ba0ece53795
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2025-06-13 17:56:37 +0200

    Split up into multiple files

---
+++ dot_config/nvim/lua/compjeuter/options.lua
@@ -0,0 +1,13 @@
+local o = vim.opt
+
+-- Line numbering
+o.number = true 
+o.relativenumber = true 
+
+
+-- Tabs
+local tabSize = 4
+
+o.expandtab = true -- replace ALL tabs with spaces, i.e. fake tabs
+o.shiftwidth = tabSize -- identation size, i.e. using `<` or `>`
+o.softtabstop = tabSize -- _fake_ tab size
