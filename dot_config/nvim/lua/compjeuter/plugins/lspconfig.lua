96911475ca53440b6f56c2b9053a6a9c9fc9a18a
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2025-06-15 16:32:59 +0200

    Added diagnostic keybind

--- dot_config/nvim/lua/compjeuter/plugins/lspconfig.lua
+++ dot_config/nvim/lua/compjeuter/plugins/lspconfig.lua
@@ -12,10 +12,16 @@ return {
             vim.keymap.set("n", binding, execFunc, options)
         end
 
+        -- Show
         set("K", vim.lsp.buf.hover)
+        set("<space>d", vim.diagnostic.open_float, opts)
+
+        -- Go to
         set("gD", vim.lsp.buf.declaration)
         set("gd", vim.lsp.buf.definition)
         set("gi", vim.lsp.buf.implementation)
+
+        -- Do
         set("<space>rn", vim.lsp.buf.rename)
         set("<space>f", function()
             vim.lsp.format {async = true}
